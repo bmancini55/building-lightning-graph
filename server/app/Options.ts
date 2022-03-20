7c138e1e551b85722a59cfcae360d2d99999daba
bmancini55 <bmancini@gmail.com>
2022-03-20 13:30:09 -0400

    add LND values to Options

--- server/app/Options.ts
+++ server/app/Options.ts
@@ -1,8 +1,12 @@
 import "dotenv/config";
+import fs from "fs/promises";
 
 export class Options {
     public static env = {
         PORT: process.env.PORT,
+        LND_HOST: process.env.LND_HOST,
+        LND_READONLY_MACAROON_PATH: process.env.LND_READONLY_MACAROON_PATH,
+        LND_CERT_PATH: process.env.LND_CERT_PATH,
     };
 
     public static async fromEnv(): Promise<Options> {
@@ -11,8 +15,19 @@ export class Options {
                 throw new Error(`Required option ENV.${key} is not defined`);
             }
         }
-        return new Options(Number(Options.env.PORT));
+
+        const port = Number(Options.env.PORT);
+        const lndHost = Options.env.LND_HOST;
+        const lndReadonlyMacaroon = await fs.readFile(Options.env.LND_READONLY_MACAROON_PATH);
+        const lndCert = await fs.readFile(Options.env.LND_CERT_PATH);
+
+        return new Options(port, lndHost, lndReadonlyMacaroon, lndCert);
     }
 
-    constructor(readonly port: number) {}
+    constructor(
+        readonly port: number,
+        readonly lndHost: string,
+        readonly lndReadonlyMacaroon: Buffer,
+        readonly lndCert: Buffer,
+    ) {}
 }

1c3dc4c1c897c871c042e67a2fe29f12d53f7109
bmancini55 <bmancini@gmail.com>
2022-03-27 12:48:45 -0400

    client: wire graph api

--- client/src/scenes/home/HomeScene.tsx
+++ client/src/scenes/home/HomeScene.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useApi } from "../../hooks/UseApi";
+import { LightningGraph } from "../../services/ApiService";
 
-export const HomeScene = () => (
-    <div className="container">
-        <h1>Welcome</h1>
-    </div>
-);
+export const HomeScene = () => {
+    const api = useApi();
+    const [graph, setGraph] = useState<LightningGraph>();
+
+    useEffect(() => {
+        api.fetchGraph().then(result => {
+            setGraph(result);
+        });
+    }, []);
+
+    return (
+        <div className="container">
+            <h1>Welcome</h1>
+        </div>
+    );
+};
--- client/src/services/ApiService.ts
+++ client/src/services/ApiService.ts
@@ -1,5 +1,16 @@
-export class ApiService {
-    constructor(readonly path: string = "http://127.0.0.1:8001") {}
+import { LightningGraph } from "../../../server/app/domain/models/LightningGraph";
 
-    // TODO Add methods
+export { LightningGraph } from "../../../server/app/domain/models/LightningGraph";
+
+export class ApiService {
+    constructor(readonly host: string = "http://127.0.0.1:8001") {}
+
+    protected async get<T>(path: string): Promise<T> {
+        const res = await fetch(path, { credentials: "include" });
+        return await res.json();
+    }
+
+    public fetchGraph(): Promise<LightningGraph> {
+        return this.get("/api/graph");
+    }
 }

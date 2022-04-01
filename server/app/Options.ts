044b9111cac336bf26e2d01d0fd0105bc58bbc66
bmancini55 <bmancini@gmail.com>
2022-03-31 23:34:14 -0400

    simplify options

--- server/app/Options.ts
+++ server/app/Options.ts
@@ -2,26 +2,13 @@ import "dotenv/config";
 import fs from "fs/promises";
 
 export class Options {
-    public static env = {
-        PORT: process.env.PORT,
-        LND_HOST: process.env.LND_HOST,
-        LND_READONLY_MACAROON_PATH: process.env.LND_READONLY_MACAROON_PATH,
-        LND_CERT_PATH: process.env.LND_CERT_PATH,
-    };
-
     public static async fromEnv(): Promise<Options> {
-        for (const [key, value] of Object.entries(Options.env)) {
-            if (!value) {
-                throw new Error(`Required option ENV.${key} is not defined`);
-            }
-        }
-
-        const port = Number(Options.env.PORT);
-        const lndHost = Options.env.LND_HOST;
-        const lndReadonlyMacaroon = await fs.readFile(Options.env.LND_READONLY_MACAROON_PATH);
-        const lndCert = await fs.readFile(Options.env.LND_CERT_PATH);
-
-        return new Options(port, lndHost, lndReadonlyMacaroon, lndCert);
+        return new Options(
+            Number(process.env.PORT),
+            process.env.LND_HOST,
+            await fs.readFile(process.env.LND_READONLY_MACAROON_PATH),
+            await fs.readFile(process.env.LND_CERT_PATH),
+        );
     }
 
     constructor(
